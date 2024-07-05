bb6c12ed272ba788df4262864dccd7e5bc356767
65_cz_lad <111596851+czLad@users.noreply.github.com>
2024-07-05 13:50:29 -0700

    Added game loop in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <iomanip>
+#include "includes/sql/sql.h"
 
 
 using namespace std;
 
 int main(int argv, char** argc) {
-    cout << "\n\n"
-         << endl;
-
-    cout << "\n\n\n=====================" << endl;
+    SQL sql;
+    Table t;
+    char s[300];
+    string s_str;
+    while(true)
+    {
+      cout<<">";
+      getline(cin, s_str);
+      strcpy(s, s_str.c_str());
+      if(s_str == "exit")
+        break;
+      Table temp = sql.command(s_str);
+      if(!sql.error_state())
+      {
+        cout<<temp;
+        if(!temp.get_tablenames_table())
+          cout << "records selected from old table: "<<sql.select_recnos() << endl;
+      }
+    }
+    cout<<"-------------Exited SQL---------------\n\n\n";
     return 0;
 }
